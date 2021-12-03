0db3e516d014b9a6ff93b91ba7cd8873364a07ce
David <davie668@gmail.com>
2021-12-03 21:02:32 +0000

    Adds throttle command

---
+++ app/commands/throttleCommand.js
@@ -0,0 +1,41 @@
+import {makeCommandString} from "./makeCommandString";
+
+/**
+ * https://dcc-ex.com/reference/software/command-reference.html#engine-decoder-cab-operation-commands
+ * @param {number} cab
+ * @param {number} speed
+ * @param {0 | 1 | -1} direction
+ * @returns {string|{returnString(): string, sendString(): string, legacy: number, cab, returnsKey: string, key: string, speed, direction}}
+ */
+export function throttleCommand({cab = 0, speed = 0, direction = 1}) {
+    return {
+        key: "t",
+        returnsKey: "T",
+        legacy: 1,
+        cab,
+        speed,
+        direction,
+        sendString() {
+            const attributes = [
+                this.key,
+                this.legacy.toString(),
+                this.cab.toString(),
+                this.speed.toString(),
+                this.direction.toString()
+            ]
+            const str = attributes.join(" ")
+            return makeCommandString(str)
+        },
+        returnString() {
+            const attributes = [
+                this.returnsKey,
+                this.legacy.toString(),
+                this.cab.toString(),
+                this.speed.toString(),
+                this.direction.toString()
+            ]
+            const str = attributes.join(" ")
+            return makeCommandString(str)
+        }
+    }
+}
---
+++ tests/unit/commands/throttleCommand.test.js
@@ -0,0 +1,63 @@
+import {throttleCommand} from "../../../app/commands/throttleCommand";
+
+const defaultOptions = {
+    options: {},
+    sendString: "<t 1 0 0 1>",
+    returnString: "<T 1 0 0 1>"
+}
+
+const cabOptions = {
+    options: {
+        cab: 22
+    },
+    sendString: "<t 1 22 0 1>",
+    returnString: "<T 1 22 0 1>"
+}
+
+const speedOptions = {
+    options: {
+        speed: 14
+    },
+    sendString: "<t 1 0 14 1>",
+    returnString: "<T 1 0 14 1>"
+}
+
+const directionOptions = {
+    options: {
+        direction: 0
+    },
+    sendString: "<t 1 0 0 0>",
+    returnString: "<T 1 0 0 0>"
+}
+
+const options = {
+    options: {
+        cab: 22,
+        speed: 14,
+        direction: 0
+    },
+    sendString: "<t 1 0 0 1>",
+    returnString: "<T 1 0 0 1>"
+}
+
+const throttleTestCases = [
+    defaultOptions,
+    cabOptions,
+    speedOptions,
+    directionOptions,
+    options
+]
+
+throttleTestCases.forEach(({options, returnString, sendString}) => {
+    test(`sendString() is '${sendString}'`, () => {
+        const command = throttleCommand(options)
+        const sendValue = command.sendString()
+        expect(sendValue).toBe(sendString)
+    })
+
+    test(`returnString() is '${returnString}'`, () => {
+        const command = throttleCommand(options)
+        const returnValue = command.returnString()
+        expect(returnValue).toBe(returnString)
+    })
+})
