e639737fb1709301296d40b118d4721411b73d2c
David <davie668@gmail.com>
2021-12-03 21:02:43 +0000

    Adds cab command

---
+++ app/commands/cabCommand.js
@@ -0,0 +1,31 @@
+import {makeCommandString} from "./makeCommandString";
+
+/**
+ * https://dcc-ex.com/reference/software/command-reference.html#cab-functions
+ * @param {number} cab
+ * @param {number} func
+ * @param {0 | 1} value
+ * @returns {string|{returnString(): string, sendString(): string, func, cab, returnsKey: string, value, key: string}}
+ */
+export function cabCommand({cab, func, value}) {
+    return {
+        key: "F",
+        returnsKey: "NONE",
+        cab,
+        func,
+        value,
+        sendString() {
+            const attributes = [
+                this.key,
+                this.cab.toString(),
+                this.func.toString(),
+                this.value.toString()
+            ]
+            const str = attributes.join(" ")
+            return makeCommandString(str)
+        },
+        returnString() {
+            return this.returnsKey;
+        }
+    }
+}
---
+++ tests/unit/commands/cabCommand.test.js
@@ -0,0 +1,22 @@
+import {cabCommand} from "../../../app/commands/cabCommand";
+
+const options = {
+    cab: 22,
+    func: 14,
+    value: 0
+}
+const sendString = "<F 22 14 0>"
+const returnString = "NONE"
+
+
+test(`sendString() is '${sendString}'`, () => {
+    const command = cabCommand(options)
+    const sendValue = command.sendString()
+    expect(sendValue).toBe(sendString)
+})
+
+test(`returnString() is '${returnString}'`, () => {
+    const command = cabCommand(options)
+    const returnValue = command.returnString()
+    expect(returnValue).toBe(returnString)
+})
